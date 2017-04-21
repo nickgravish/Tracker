9d755ebc882e8889c006a06b24b5bd52acae7992
Nick Gravish <nickgravish@Nicks-iMac.local>
2017-04-21 16:50:00 -0700

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Tracker',
     version='0.1dev',
-    packages=['',],
+    packages=['Tracker',],
     license='MIT',
     long_description=open('README.md').read(),
 )
